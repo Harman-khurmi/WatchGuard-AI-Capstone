63b0e102bb3a15e35fb375ae2dd1796ad1f553c3
Harman-khurmi <hsingh2_be21@thapar.edu>
2024-12-17 11:24:26 +0530

    changes 1

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,7 +1,7 @@
 export const navItems = [
-  { label: "Dawdle Insights", href: "https://insights.dawdle.live/" },
-  { label: "Consultants", href: "https://consultant.dawdle.live/" },
-  { label: "Companies", href: "https://cal.com/dawdle-clients/exploratory" },
+  { label: "abc", href: "" },
+  { label: "xyz", href: "" },
+  { label: "pqr", href: "" },
 ];
 export const footerItems = [
   { label: "Privacy Policy", href: "#" },
